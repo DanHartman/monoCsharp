75fda2e2b7813f71f7873aa455443f2c917c5ad2
Dan Hartman <dan.h@rtman.org>
2018-01-26 17:06:21 -0600

    Add default value if no argument passed

--- src/helloworld.cs
+++ src/helloworld.cs
@@ -1,6 +1,7 @@
 public class HelloWorld {
   public static void Main(string[] args) {
-      System.Console.WriteLine($"Hello, {args[0]}!");
+  	  string name = (args == null || args.Length == 0) ? "Geoffery" : args[0];
+      System.Console.WriteLine($"Hello, {name}!");
   }
 }
 
